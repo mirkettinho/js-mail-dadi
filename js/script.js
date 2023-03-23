50b39f92f399f72a7aac016cb1fe2e4f0610db2d
mirkettinho <mirkodifranco30@gmail.com>
2023-03-23 18:23:32 +0100

    [add] css

--- js/script.js
+++ js/script.js
@@ -33,22 +33,39 @@ if (userEmail === ""){
 const pc = [1,2,3,4,5,6];
 const human = [1,2,3,4,5,6]
 
+let risultato = document.querySelector(".risultato")
+console.log(risultato)
+
+let redCorner = document.querySelector(".red")
+
+let blueCorner = document.querySelector(".blue")
+
+
 let randomNumberPc = Math.floor(Math.random()*pc.length + 1);
+blueCorner.innerHTML = randomNumberPc
+
 
 
 let randomNumberHuman = Math.floor(Math.random()*human.length + 1 );
+redCorner.innerHTML = randomNumberHuman
+
 
 console.log("pc " + randomNumberPc)
 console.log("player " + randomNumberHuman)
 
 if (randomNumberPc === randomNumberHuman){
   console.log("pareggio")
+  risultato.innerHTML = `NULLA`
 }
 
 if (randomNumberPc > randomNumberHuman){
   console.log("il vincitore è il pc")
+  risultato.innerHTML = `PC`
 }
 
 if (randomNumberPc < randomNumberHuman){
   console.log("il vincitore è l'umano")
+  risultato.innerHTML = `UMANO`
 }
+
+
